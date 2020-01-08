fff412509acb339a728d91cacb304bd5a2418649
Rogelio Cobian <rogeliocobian2@gmail.com>
2020-01-08 10:21:48 -0800

    added single.php for post display and page.php for pages

--- wordpress/wp-content/themes/fictional-university-theme/page.php
+++ wordpress/wp-content/themes/fictional-university-theme/page.php
@@ -2,7 +2,7 @@
 
 	while(have_posts()) {
 		the_post(); ?>
-		<h2> <a href= "<?php the_permalink(); ?>"> <?php the_title(); ?> </a></h2>
+		<h1> This is a page not a post </h1>
 		<p> <?php the_content();?> </p>
 		<hr>
 	<?php }
